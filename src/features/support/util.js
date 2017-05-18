6c36e805fe92222a13d50d624e7e7ccee74b175d
Vit Stanislav <vit@lightcurve.io>
2017-05-18 15:44:23 +0200

    Fix when assynchronous callbacks are called
    .. in e2e test support functions

--- src/features/support/util.js
+++ src/features/support/util.js
@@ -10,30 +10,33 @@ function waitForElemAndCheckItsText(selector, text, callback) {
   const elem = element(by.css(selector));
   browser.wait(EC.presenceOf(elem), waitTime, `waiting for element '${selector}'`);
   expect(elem.getText()).to.eventually.equal(text, `inside element "${selector}"`)
-    .and.notify(callback);
+    .and.notify(callback || (() => {}));
 }
 
 function waitForElemAndClickIt(selector, callback) {
   const elem = element(by.css(selector));
   browser.wait(EC.presenceOf(elem), waitTime, `waiting for element '${selector}'`);
-  elem.click();
-  if (callback) callback();
+  elem.click().then(() => {
+    if (callback) callback();
+  });
 }
 
 function waitForElemAndSendKeys(selector, keys, callback) {
   const elem = element(by.css(selector));
   browser.wait(EC.presenceOf(elem), waitTime, `waiting for element '${selector}'`);
-  elem.sendKeys(keys);
-  if (callback) callback();
+  elem.sendKeys(keys).then(() => {
+    if (callback) callback();
+  });
 }
 
 function checkAlertDialog(title, text, callback) {
   waitForElemAndCheckItsText('md-dialog h2', title);
   waitForElemAndCheckItsText('md-dialog .md-dialog-content-body', text);
   const okButton = element(by.css('md-dialog .md-button.md-ink-ripple'));
-  okButton.click();
-  browser.sleep(500);
-  if (callback) callback();
+  okButton.click().then(() => {
+    browser.sleep(500);
+    if (callback) callback();
+  });
 }
 
 module.exports = {
